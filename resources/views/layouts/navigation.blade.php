12bf6e2a2148cc73486caf4bfda8567e276eb93c
Mdb713 <Marianodominguez456@gmail.com>
2025-12-09 13:26:27 +0100

    Se hacen los utlimos cambios
    Se hacen los utlimos cambios

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -31,8 +31,8 @@
 
     <script src="{{ asset('js/footer.js') }}" defer></script>
     <script src="{{ asset('js/navbar.js') }}" defer></script>
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.0/css/all.min.css"
-        integrity="sha512-..." crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.0/css/all.min.css">
+
     <link rel="icon" href="{{ asset('images/logo.png') }}" type="image/png">
     <script type="text/javascript" src="https://cdn.jsdelivr.net/npm/@emailjs/browser@3/dist/email.min.js"></script>
 </head>
